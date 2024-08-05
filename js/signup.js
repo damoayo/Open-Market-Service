4954cbf7e0e1dbc5050b034199f10616c78b52d8
mamapai <mamapai@naver.com>
2024-08-05 14:17:32 +0900

    회원가입 UI

---
+++ js/signup.js
@@ -0,0 +1,23 @@
+function openTab(tabName) {
+  const addBusiness = document.getElementById('add-Business');
+  const addStoreName = document.getElementById('add-storeName');
+  const tabLeft = document.querySelector('.tabLeft');
+  const tabRight = document.querySelector('.tabRight');
+  const wrapForm = document.getElementsByClassName('wrap-form');
+
+  if (tabName === 'buyer') {
+    addBusiness.style.display = 'none';
+    addStoreName.style.display = 'none';
+    tabLeft.classList.add('active');
+    tabRight.classList.remove('active');
+    wrapForm[0].style.height = '586px';
+  } else if (tabName === 'seller') {
+    addBusiness.style.display = 'block';
+    addStoreName.style.display = 'block';
+    tabRight.classList.add('active');
+    tabLeft.classList.remove('active');
+    wrapForm[0].style.height = '820px';
+  }
+}
+
+openTab('buyer');
