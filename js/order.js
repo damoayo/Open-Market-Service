889b5bae9f119edaaf138c704085706cd3c04758
mamapai <mamapai@naver.com>
2024-08-15 16:29:32 +0900

    주문페이지 주문입력검사

--- js/order.js
+++ js/order.js
@@ -4,124 +4,125 @@
 function changeColor(element) {
   const text = document.getElementById(element.id);
   // const cartLink = document.getElementById('cartBtn');
-  const img = text.getElementsByTagName('img')[0];
+  const img = text.getElementsByTagName("img")[0];
   // const cartImg = cartLink.getElementsByTagName('img')[0];
 
   // "로그인-클릭" icon
-  if (element.id === 'loginBtn') {
-    img.src = './assets/icon-user-2.svg';
-    img.style.paddingLeft = '12px';
+  if (element.id === "loginBtn") {
+    img.src = "./assets/icon-user-2.svg";
+    img.style.paddingLeft = "12px";
   } else {
-    img.src = './assets/icon-shopping-cart-2.svg';
-    img.style.paddingLeft = '3px';
+    img.src = "./assets/icon-shopping-cart-2.svg";
+    img.style.paddingLeft = "3px";
   }
-  img.style.width = '32px';
-  img.style.height = '32px';
+  img.style.width = "32px";
+  img.style.height = "32px";
 
   // "로그인-클릭" 텍스트
-  text.style.color = 'rgba(33, 191, 72, 1)';
-  text.style.fontSize = '12px';
-  text.style.fontWeight = '400';
-  text.style.width = '56px';
-  text.style.height = '14px';
+  text.style.color = "rgba(33, 191, 72, 1)";
+  text.style.fontSize = "12px";
+  text.style.fontWeight = "400";
+  text.style.width = "56px";
+  text.style.height = "14px";
 }
 // 로그인 icon 함수
 function reChangeColor(element) {
   const text = document.getElementById(element.id);
-  const img = text.getElementsByTagName('img')[0];
+  const img = text.getElementsByTagName("img")[0];
   // console.log(img);
-  if (element.id === 'loginBtn') {
-    img.src = './assets/icon-user.svg';
-    img.style.paddingLeft = '12px';
+  if (element.id === "loginBtn") {
+    img.src = "./assets/icon-user.svg";
+    img.style.paddingLeft = "12px";
   } else {
-    img.src = './assets/icon-shopping-cart.svg';
-    img.style.paddingLeft = '3px';
+    img.src = "./assets/icon-shopping-cart.svg";
+    img.style.paddingLeft = "3px";
   }
 
-  img.style.width = '32px';
-  img.style.height = '32px';
+  img.style.width = "32px";
+  img.style.height = "32px";
 
-  text.style.color = 'rgba(118, 118, 118, 1)';
-  text.style.fontSize = '12px';
-  text.style.fontWeight = '400';
-  text.style.width = '56px';
-  text.style.height = '14px';
+  text.style.color = "rgba(118, 118, 118, 1)";
+  text.style.fontSize = "12px";
+  text.style.fontWeight = "400";
+  text.style.width = "56px";
+  text.style.height = "14px";
 }
 // 로그아웃 icon 함수
 function logoutIconColor(element) {
   const text = document.getElementById(element.id);
-  const img = text.getElementsByTagName('img')[0];
+  const img = text.getElementsByTagName("img")[0];
   // console.log(img);
-  if (element.id === 'loginBtn') {
-    img.src = './assets/icon-user.svg';
-    img.style.paddingLeft = '2px';
+  if (element.id === "loginBtn") {
+    img.src = "./assets/icon-user.svg";
+    img.style.paddingLeft = "2px";
   } else {
-    img.src = './assets/icon-shopping-cart.svg';
-    img.style.paddingLeft = '3px';
+    img.src = "./assets/icon-shopping-cart.svg";
+    img.style.paddingLeft = "3px";
   }
 
-  img.style.width = '32px';
-  img.style.height = '32px';
+  img.style.width = "32px";
+  img.style.height = "32px";
 
-  text.style.color = 'rgba(118, 118, 118, 1)';
-  text.style.fontSize = '12px';
-  text.style.fontWeight = '400';
-  text.style.width = '56px';
-  text.style.height = '14px';
+  text.style.color = "rgba(118, 118, 118, 1)";
+  text.style.fontSize = "12px";
+  text.style.fontWeight = "400";
+  text.style.width = "56px";
+  text.style.height = "14px";
 }
-
 
 // 로그인 상태일 때 네비게이션 업데이트
 function updateNavLogin() {
-  const loginLink = document.getElementById('loginBtn');
-  const cartLink = document.getElementById('cartBtn');
-  const dropdownMenu = document.getElementById('dropdownMenu');
-  const logoutBtn = document.getElementById('logoutBtn');
-  const loginImg = loginLink.querySelector('img');
+  const loginLink = document.getElementById("loginBtn");
+  const cartLink = document.getElementById("cartBtn");
+  const dropdownMenu = document.getElementById("dropdownMenu");
+  const logoutBtn = document.getElementById("logoutBtn");
+  const loginImg = loginLink.querySelector("img");
 
   // 로그인 버튼을 마이페이지로 변경
-  loginLink.innerHTML = '<img src="./assets/icon-user.svg" alt="마이페이지" /> 마이페이지';
+  loginLink.innerHTML =
+    '<img src="./assets/icon-user.svg" alt="마이페이지" /> 마이페이지';
   reChangeColor(loginLink);
   reChangeColor(cartLink);
   // 변경된 버튼의 style과 기본작동
-  loginLink.addEventListener('click', (event) => {
+  loginLink.addEventListener("click", (event) => {
     event.preventDefault(); // 기본 링크 동작을 막음
     changeColor(loginLink);
     // changeColor(cartLink);
     // 드롭다운 박스의 표시 상태를 토글
-    dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
+    dropdownMenu.style.display =
+      dropdownMenu.style.display === "block" ? "none" : "block";
   });
 
   // 이미지 클릭 시에도 버튼과 같은효과
-  loginImg.addEventListener('click', (event) => {
+  loginImg.addEventListener("click", (event) => {
     event.preventDefault(); // 기본 링크 동작을 막음
     loginLink.click();
   });
 
   // 외부 클릭 시 드롭다운 박스 닫기
-  window.addEventListener('click', (event) => {
-    if (!event.target.closest('#loginBtn')) {
+  window.addEventListener("click", (event) => {
+    if (!event.target.closest("#loginBtn")) {
       reChangeColor(loginLink);
       reChangeColor(cartLink);
-      if (dropdownMenu.style.display === 'block') {
-        dropdownMenu.style.display = 'none';
+      if (dropdownMenu.style.display === "block") {
+        dropdownMenu.style.display = "none";
       }
     }
   });
 
   // 드롭다운 박스 클릭 시 이벤트 버블링 방지
   // 내부 클릭 시에도 드롭다운 박스가 닫히지 않도록 정상동작 보장
-  dropdownMenu.addEventListener('click', (event) => {
+  dropdownMenu.addEventListener("click", (event) => {
     event.stopPropagation(); // 클릭 이벤트가 window로 전파중지 이벤트
   });
 
   // 로그아웃 버튼 클릭 시 로그아웃 처리
-  logoutBtn.addEventListener('click', () => {
+  logoutBtn.addEventListener("click", () => {
     // 스토리지에서 토큰과 isLoggedIn 제거
-    localStorage.removeItem('token');
-    localStorage.removeItem('isLoggedIn');
+    localStorage.removeItem("token");
+    localStorage.removeItem("isLoggedIn");
     // 로그인 페이지로 리디렉션 (예: login.html)
-    window.location.href = 'index.html';
+    window.location.href = "index.html";
   });
 }
 
@@ -129,39 +130,41 @@ function updateNavLogin() {
 
 async function fetchCart() {
   try {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
 
     if (!token) {
-      document.getElementById('productMessage').textContent = '로그인이 필요합니다!';
+      document.getElementById("productMessage").textContent =
+        "로그인이 필요합니다!";
       window.history.back();
       return; // 함수 실행 중단
     }
 
-    const res = await fetch('https://openmarket.weniv.co.kr/cart/', {
-      method: 'GET',
+    const res = await fetch("https://openmarket.weniv.co.kr/cart/", {
+      method: "GET",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
         Authorization: `JWT ${token}`,
       },
-      credentials: 'include',
+      credentials: "include",
     });
 
     if (!res.ok) {
-      throw new Error('네트워크 응답이 올바르지 않습니다.');
+      throw new Error("네트워크 응답이 올바르지 않습니다.");
     }
 
     const data = await res.json();
     handleCartData(data);
   } catch (error) {
-    console.error('제품 목록 요청 중 오류 발생:', error);
-    document.getElementById('productMessage').textContent = '제품 목록 요청 중 오류가 발생했습니다. 다시 시도해주세요.';
+    console.error("제품 목록 요청 중 오류 발생:", error);
+    document.getElementById("productMessage").textContent =
+      "제품 목록 요청 중 오류가 발생했습니다. 다시 시도해주세요.";
   }
 }
 
 function displayEmptyCartMessage() {
-  const productMessage = document.getElementById('productMessage');
-  productMessage.innerHTML = '';
-  const emptyCartMessage = document.createElement('div');
+  const productMessage = document.getElementById("productMessage");
+  productMessage.innerHTML = "";
+  const emptyCartMessage = document.createElement("div");
   emptyCartMessage.innerHTML = `
     <div id="productMessage">
       <p>장바구니에 담긴 상품이 없습니다.</p>
@@ -173,7 +176,7 @@ function displayEmptyCartMessage() {
 
 // 로컬스토리지와 서버에서 가져온 데이터를 확인
 function handleCartData(data) {
-  const cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
+  const cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
   const cartItemsQuan = cartItems.length;
   const cartData = data.results || [];
 
@@ -181,19 +184,21 @@ function handleCartData(data) {
     displayCartItems(cartItems, cartData);
   } else {
     displayEmptyCartMessage();
-
   }
 }
 
 async function fetchProductDetails(product_id) {
   try {
-    const res = await fetch(`https://openmarket.weniv.co.kr/products/${product_id}/`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      credentials: 'include',
-    });
+    const res = await fetch(
+      `https://openmarket.weniv.co.kr/products/${product_id}/`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+      }
+    );
 
     if (res.ok) {
       const data = await res.json();
@@ -225,7 +230,7 @@ async function displayCartItems(cartItems, cartData) {
   const filteredItems = Object.values(uniqueItems);
   const productMessage = document.getElementById("productMessage");
   productMessage.innerHTML = "";
-
+  // 상품 ID만 추출
   const filteredArr = filteredItems.map((item) => item.productId);
   // undefined 또는 null 값 제거
   let filteredIds = filteredArr.filter((product) => product !== undefined);
@@ -234,7 +239,6 @@ async function displayCartItems(cartItems, cartData) {
   const productDetailsPromises = filteredIds.map((item) =>
     fetchProductDetails(item)
   );
-
   // 비동기작업을 병렬로 실행하여 모두완료가 된후에 배열로 반환
   let productDetails = await Promise.all(productDetailsPromises); // Promise.all()은 모든 Promise가 완료될 때까지 기다림
   const cartList = document.getElementById("cartList");
@@ -245,6 +249,7 @@ async function displayCartItems(cartItems, cartData) {
     document.getElementById("totalProductAmount");
   const finalAmountElement = document.getElementById("finalAmount");
 
+  // 상품 정보와 장바구니 정보를 합쳐서 화면에 표시
   productDetails.forEach((product, index) => {
     if (product) {
       const item = filteredItems[index];
@@ -279,43 +284,9 @@ async function displayCartItems(cartItems, cartData) {
         </div>
       </div>
     `;
-    
 
+      // 장바구니 목록에 상품 추가
       cartList.appendChild(itemElement);
-
-      // 요소 선택
-      const checkbox = itemElement.querySelector('.chk');
-      const deleteModal = document.getElementById('deleteModal');
-      const confirmDeleteBtn = document.getElementById('confirmDelete');
-      const cancelDeleteBtn = document.getElementById('cancelDelete');
-      const decreaseQtyBtn = itemElement.querySelector('.minus');
-      const increaseQtyBtn = itemElement.querySelector('.plus');
-      const quantitySpan = itemElement.querySelector('.quantity');
-      const sumPriceElement = itemElement.querySelector('.sum-price');
-
-      let currentQuantity = item.quantity;
-
-      // 수량 감소 이벤트
-      decreaseQtyBtn.addEventListener('click', function () {
-        if (currentQuantity > 1) {
-          currentQuantity--;
-          quantitySpan.innerText = currentQuantity;
-          updateTotalPrice(product.price, currentQuantity, sumPriceElement);
-          updateCartTotals();
-        }
-      });
-
-      // 수량 증가 이벤트
-      increaseQtyBtn.addEventListener('click', function () {
-        if (currentQuantity < product.stock) {
-          currentQuantity++;
-          quantitySpan.innerText = currentQuantity;
-          updateTotalPrice(product.price, currentQuantity, sumPriceElement);
-          updateCartTotals();
-        }
-      });
-
-
 
       // 각 상품의 합계 금액을 업데이트하는 함수
       function updateTotalPrice(price, quantity, element) {
@@ -328,8 +299,10 @@ async function displayCartItems(cartItems, cartData) {
         totalProductAmount = 0;
 
         // 모든 cart-item 요소를 순회하면서 각 항목의 가격을 합산
-        document.querySelectorAll('.sum-price').forEach((sumPriceElement) => {
-          const itemTotal = parseInt(sumPriceElement.innerText.replace(/,/g, '').replace('원', '')); // 문자열을 숫자로 변환
+        document.querySelectorAll(".sum-price").forEach((sumPriceElement) => {
+          const itemTotal = parseInt(
+            sumPriceElement.innerText.replace(/,/g, "").replace("원", "")
+          ); // 문자열을 숫자로 변환
           totalProductAmount += itemTotal;
         });
 
@@ -345,17 +318,100 @@ async function displayCartItems(cartItems, cartData) {
 
 // 로그인 상태 확인 및 네비게이션 업데이트
 function checkLoginStatus() {
-  const isLoggedIn = localStorage.getItem('isLoggedIn');
+  const isLoggedIn = localStorage.getItem("isLoggedIn");
 
-  if (isLoggedIn === 'true') {
+  if (isLoggedIn === "true") {
     updateNavLogin();
     fetchCart();
   } else {
-    window.location.href = 'login.html';
+    window.location.href = "login.html";
     return;
   }
 }
 // 페이지 로드 시 로그인 상태 확인
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   checkLoginStatus();
 });
+
+/* ######################################  주문정보 입력부분 */
+document.addEventListener("DOMContentLoaded", function () {
+  const orderForm = document.getElementById("orderForm");
+  const paymentButton = document.querySelector(".payment-button");
+  const agreeTerms = document.getElementById("agreeTerms");
+  const phoneInputs = document.querySelectorAll(
+    "#ordererPhone1, #ordererPhone2, #ordererPhone3, #recipientPhone1, #recipientPhone2, #recipientPhone3"
+  );
+  const emailInput = document.getElementById("ordererEmail");
+  const emailValidate = document.getElementById("emailValidate");
+
+  // phone input 필드에 숫자만 입력할 수 있도록 제한
+  phoneInputs.forEach((input) => {
+    input.addEventListener("input", function () {
+      input.value = input.value.replace(/[^0-9]/g, ""); // 숫자가 아닌 문자는 제거
+    });
+  });
+
+  // 이메일 형식 검사
+  function isValidEmail(email) {
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailPattern.test(email);
+  }
+
+  // 폼의 모든 필드와 라디오 버튼을 확인하는 함수
+  function checkFormCompletion() {
+    const requiredInputs = orderForm.querySelectorAll("input[required]");
+    const selectedPaymentMethod = orderForm.querySelector(
+      "input[name='payment_method']:checked"
+    );
+    const emailValid = isValidEmail(emailInput.value.trim());
+
+    let allFieldsFilled = true;
+    requiredInputs.forEach((input) => {
+      if (!input.value) {
+        allFieldsFilled = false;
+      }
+    });
+
+    // 체크박스가 선택되었는지 확인
+    if (
+      allFieldsFilled &&
+      selectedPaymentMethod &&
+      agreeTerms.checked &&
+      emailValid
+    ) {
+      paymentButton.disabled = false;
+      paymentButton.classList.add("enabled");
+      paymentButton.classList.remove("disabled");
+    } else {
+      paymentButton.disabled = true;
+      paymentButton.classList.add("disabled");
+      paymentButton.classList.remove("enabled");
+    }
+  }
+  // 입력 필드, 라디오 버튼, 체크박스에 이벤트 리스너 추가
+  const allInputs = orderForm.querySelectorAll("input");
+  allInputs.forEach((input) => {
+    input.addEventListener("input", checkFormCompletion);
+  });
+
+  const paymentMethods = orderForm.querySelectorAll(
+    "input[name='payment_method']"
+  );
+  paymentMethods.forEach((radio) => {
+    radio.addEventListener("change", checkFormCompletion);
+  });
+  agreeTerms.addEventListener("change", checkFormCompletion);
+
+  // 이메일 입력 필드에서 포커스 잃을 때 검사
+  emailInput.addEventListener("blur", function () {
+    if (!isValidEmail(emailInput.value.trim())) {
+      emailValidate.innerText = "이메일 형식이 올바르지 않습니다.";
+      emailInput.focus();
+    } else {
+      emailValidate.innerText = "";
+    }
+  });
+});
+
+/* ######################################  주문정보 끝 */
+
